2deaa32413f91cbb18e197f5f1bcb64bd20c16ee
sptungG <ngoviettung154@gmail.com>
2021-08-12 13:21:40 +0700

    footer done

--- HTML-CSS-JS-PSD/PSD-to-HTML-with-Gulp-Pug-SASS/00-PSD-to-HTML-course/app/scripts/main.js
+++ HTML-CSS-JS-PSD/PSD-to-HTML-with-Gulp-Pug-SASS/00-PSD-to-HTML-course/app/scripts/main.js
@@ -8,3 +8,17 @@ window.addEventListener("click", function (e) {
     menu.classList.remove("is-expand");
   }
 });
+$(document).ready(function () {
+  $(".quote-container").slick({
+    prevArrow: `<button type="button" class="slick-arrow slick-prev"><i class = "fa fa-angle-left"></i></button>`,
+    nextArrow: `<button type="button" class="slick-arrow slick-next"><i class = "fa fa-angle-right"></i></button>`,
+    responsive: [
+      {
+        breakpoint: 767,
+        settings: {
+          arrows: false,
+        },
+      },
+    ],
+  });
+});
